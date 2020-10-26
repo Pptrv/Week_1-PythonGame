818cb446dd4aa892d5b01963bc2dfe2788119df7
pptrv <trv.pepe@gmail.com>
2020-10-26 01:22:37 +0100

    juegofin

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# Week_1-PythonGame
+## Week_1-PythonGame
 
 Welcome to the Captain Long John Silver´s Hangman Game 
 
-**Objetive of the Game**
+#**Objetive of the Game**
 
 Guess the word before the Captain gets hung!
 
-**Rules of the Game**
+#**Rules of the Game**
 
 1. One player or in groups
-2. A random word will be selected from the pirate.file
+2. A random word will be selected from the pirate's dictionary
 3. You must choose a letter at a time to fill the blank spaces of the missing word. 
-4. If you choose 5 words, that are not in the word you must guess, the Captain will die and you will lose the game. 
+4. If you choose 5 letters, that are not in the word, the Captain dies and you lose. 
 
 **Good luck saving Captain Long John Silver**
--- main.py
+++ main.py
@@ -2,28 +2,41 @@ import random
 MAX_MISSES = 5
 
 def print_welcome():
-    print('Welcome to Hangman!')
+    # Mensaje de Inicio e instrucciones
+    print('')
+    print('')
+    print('')
+    print('Welcome to John Long Silver Hangman Game!')
+    print('Guess the word missing in the blank spaces')
 
 def print_round_start():
+    # Separador para el inicio de partida. 
     print('')
-    print('------------')
+    print('---------------------------------------------------------------')
 
 def print_round_finish(correct):
+    # Recoge el input en caso de no darse las condiciones para ganar o perder el juego, para mostrar si hemos acertado o no la palabra. 
+
     if correct:
         print('Correct!')
     else:
         print('Shame...')
-    print('------------')
+    print('---------------------------------------------------------------')
     print('')
 
 def print_lives_left(number_of_misses):
+    # Entra el input de "MAX_MISSES" menos "number_of_misses", se printea el numero de vidas restantes. 
     print(f'You have {MAX_MISSES-number_of_misses} lives left...')
 
 def get_input(previous_guesses):
+    # Mientras la lista de "previous_guesses" no tenga todas las letras de "word" el loop continua y sigue pidiendo un input ("guess") que se añade a la lista "previous_gusses". 
+    # El input se transforma en minusculas para poder comparar con "word". 
+    # El input solo acepta 1 letra, en caso de ser más de una, sale un aviso y no te quitan vidas.
+
     is_valid = False
 
     while(is_valid is False):
-        guess = input('--> ')
+        guess = input('letter: ')
         guess = guess.lower()
 
         if guess in previous_guesses:
@@ -37,10 +50,13 @@ def get_input(previous_guesses):
     return guess, previous_guesses
 
 def get_random_word():
+    # Selecciona una palabra del diccionario de piratas y la devuelve en letras minusculas. 
     words = open('dictionary.txt').read().splitlines()
     return random.choice(words).lower()
 
 def main():
+    # Funcion en loop para llamar al juego mientras no ganemos o perdamos. 
+
     print_welcome()
 
     number_of_misses = 0
@@ -53,25 +69,34 @@ def main():
     lose_condition = False
 
     while(not win_condition and not lose_condition):
+        # Al asignar las variables como falsas, hasta que alguna de las dos condicioens no se cumpla, el juego continua. 
         print_round_start()
+        # Printea un separador.
         print_lives_left(number_of_misses)
+        # Entra el input de "MAX MISSES" menos "number_of_misses", se printea el numero de vidas restantes. 
 
         guessed_word = ''.join([i if i in previous_guesses else '_' for i in word])
+        # Se crea una string de la lista de "previous_guesses" y espacios '_' para el resto de letras que siguen sin acertarse en la variable "word".
         print(guessed_word.capitalize())
+        # Imprimimos la primera letra de "gussed_word" en mayusculas. 
 
         guess, previous_guesses = get_input(previous_guesses)
 
+
         if guess in word:
+            # Mientras "guessed_word != word el win_condition no se printea True y el juego continua.
             is_right_guess = True
             guessed_word = ''.join([i if i in previous_guesses else '_' for i in word])
             win_condition = (guessed_word == word)
 
         else:
+            # Mientras "lose_contidion" no sea true, loop continua y se resta una vida. 
             is_right_guess = False
             number_of_misses += 1
             lose_condition = (number_of_misses >= MAX_MISSES)
 
         print_round_finish(is_right_guess)
+        # La ronda continua, printeando si han acertado o fallado
 
     if win_condition:
         print('YOU WIN!')
